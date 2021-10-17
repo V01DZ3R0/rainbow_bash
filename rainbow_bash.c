ef51241a183c39af5a784d6e8f3df60bd0b77ae0
V01DZ3R0 <91835641+V01DZ3R0@users.noreply.github.com>
2021-10-17 21:11:58 +0300

    gotta make string input...
    Removed unused code

--- rainbow_bash.c
+++ rainbow_bash.c
@@ -1,7 +1,6 @@
 //Got the pun?
 
 #include <stdio.h>
-// #include <string.h> // for strlen, coz sizeof()-1 does not work here
 #include <time.h> //for nanosleep
 int main(void)
 {
@@ -9,31 +8,20 @@ int main(void)
     int i, j, k, l;
 
     struct timespec wait = {0, 1000000}, left; //delay time is set in nano-seconds, variable "left" is useless
-                                               /*
+    
+    /*
    colors
-
-   order of color like in rainbow -- it's number in  \033[3Xm  (instead of X)
+   order of color like in rainbow -- its' number in  \033[3Xm  (instead of X)
                                 1 -- 1
                                 2 -- 3
                                 3 -- 2
                                 4 -- 6
                                 5 -- 4
                                 6 -- 5    
-    */
-
-    /*
-    //string input - needs fixin'
-    char clr[] = "132645";
-    printf("Write your string to rainbownize:\n");
-    char input_str[100];
-     scanf("%s", input_str);
-     char str[strlen(input_str)];
-
-     strcpy(str,input_str);
-
      */
 
-    char str[] = "RAINBOW";
+
+    char str[] = "RAINBOW"; //Or any other string you like
 
     char clr[] = "546231"; //order is reversed because i said so.
     while (69)
@@ -59,29 +47,7 @@ int main(void)
 
     printf("\n");
 
-    //ннада добавить вощще обнуление этих цветов ибаных
+   
     return 0;
 }
 
-void cycle() //тут "бесконечность" без обнуления двух размеров (текста и параметра) работает, а у выше нет, ннада пофиксить
-{
-    int i, j, k;
-
-    char test[] = " A B C D E F G";
-
-    j = sizeof(test) - 1;
-
-    for (k = 0; k <= j; k++) // меньше или равно (<=) только чтобы выводил изначальную строчку и доказывал возможность бесконечного прогона цикла и его блять красоту!
-    {
-        for (i = 0; i < j; i++)
-        {
-            if (i + k >= j)
-            {
-                printf("%c", test[i + k - j]);
-                continue;
-            }
-            printf("%c", test[i + k]);
-        }
-        printf("\n");
-    }
-}
